1d6660ebfcbee3e1435eb5a9c39bf072a3ca3836
solettanitm <solettanitm@gmail.com>
2017-07-04 10:09:29 +0200

    Add files via upload

--- merge_date.sh
+++ merge_date.sh
@@ -1,15 +1,49 @@
-#!/bin/bash
+#!/usr/local/bin/bash
 
 #query earliest start date and latest end date
 function merge_time()
 {
-    result=$(sqlite3 data_events.sqlite "select min(data_since), max(data_until) from data_events where data_source_account_id = $account and data_since >= '$start'")
+    result=$(sqlite3 data_events.sqlite "select data_since, data_until from data_events where data_source_account_id = $account and data_since >= '$start' order by data_until")
+    
     #output format
     if [ -z "$result" -o "$result" = "|" ] ; then
         echo "Nothing found."
     else
-        echo "Time interval is from ${result//|/ to }"
+        echo "Time interval is:"
+        result=${result// 00:00:00.000000/}
+        arrR=(${result// / })
+        declare -A mergedmap=()
+        
+        index=0
+        begin=""
+        temp=""
+        for i in ${arrR[@]}
+        do
+            #echo $i
+            arrT=(${i//|/ })
+            #echo "${arrT[0]} to ${arrT[1]}" 
+            if [ $index -eq 0 ]; then
+                mergedmap["${arrT[0]}"]="${arrT[1]}"
+                begin=${arrT[0]}
+                temp=${arrT[1]}
+            else
+               if [ ${arrT[0]} \< $temp -o ${arrT[0]} = $temp ]; then
+                   mergedmap[$begin]="${arrT[1]}"
+                   temp=${arrT[1]}
+               else
+                   mergedmap[${arrT[0]}]="${arrT[1]}"
+               fi
+            fi         
+            index=$((index+1))           
+        done
+    
     fi
+
+    #print out
+    for key in ${!mergedmap[@]}
+    do
+        echo "${key} to ${mergedmap[$key]}"
+    done
 }
 
 read -t 15 -p "input your account ID and start date (e.g. 2017-01-21): " account start
